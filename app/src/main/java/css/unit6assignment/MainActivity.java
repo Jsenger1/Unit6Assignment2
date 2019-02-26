da44f311cf615ed20544905b215fff36a75fe62f
jsenger1 <jsenger1@css.edu>
2019-02-26 12:33:45 -0600

    changes to oncreate

--- app/src/main/java/css/unit6assignment/MainActivity.java
+++ app/src/main/java/css/unit6assignment/MainActivity.java
@@ -22,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setupButtonClickEvents();
+        setupButtonClickEvents2();
+        setupButtonClickEvents3();
 
     }
 
